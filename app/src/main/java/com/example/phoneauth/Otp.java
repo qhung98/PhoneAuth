a421f6c55cb77edbf14f5668a5e6415586639088
qhung98 <qhung0809@gmail.com>
2020-07-03 23:59:44 +0700

    Initial commit

---
+++ app/src/main/java/com/example/phoneauth/Otp.java
@@ -0,0 +1,93 @@
+package com.example.phoneauth;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.PhoneAuthCredential;
+import com.google.firebase.auth.PhoneAuthProvider;
+
+public class Otp extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+    private FirebaseUser mCurrentUser;
+
+    private String mAuthVerificationId;
+
+    private EditText mOtpText;
+    private Button mVerifyBtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_otp);
+
+        mAuth = FirebaseAuth.getInstance();
+        mCurrentUser = mAuth.getCurrentUser();
+
+        mAuthVerificationId = getIntent().getStringExtra("AuthCredentials");
+
+        mOtpText = findViewById(R.id.otp_text_view);
+
+        mVerifyBtn = findViewById(R.id.verify_btn);
+
+        mVerifyBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String otp = mOtpText.getText().toString();
+
+                if(otp.isEmpty()){
+                    Toast.makeText(getBaseContext(), "NOT EMPTY OTP", Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    PhoneAuthCredential credential = PhoneAuthProvider.getCredential(mAuthVerificationId, otp);
+                    signInWithPhoneAuthCredential(credential);
+                }
+            }
+        });
+    }
+
+    private void signInWithPhoneAuthCredential(PhoneAuthCredential credential) {
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(Otp.this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            sendUserToHome();
+                        } else {
+                            if (task.getException() instanceof FirebaseAuthInvalidCredentialsException) {
+                              Toast.makeText(getBaseContext(), "OTP ERROR", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    }
+                });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        if(mCurrentUser != null){
+            sendUserToHome();
+        }
+    }
+
+    private void sendUserToHome() {
+        Intent homeIntent = new Intent(Otp.this, MainActivity.class);
+        homeIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        homeIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(homeIntent);
+        finish();
+    }
+}
